64605d931c38094ceda2717691a5d0a8c7a2bf4d
Patric Khiev <pat@Patrics-MacBook-Pro.local>
2018-11-10 23:07:38 -0600

    scanner modal upgraded

--- src/Components/DLRenewal/DLScan/DLScan.js
+++ src/Components/DLRenewal/DLScan/DLScan.js
@@ -3,57 +3,44 @@ import "./DLScan.scss";
 import FileBase64 from "react-file-base64";
 import axios from "axios";
 import UserSideNav from "../../SideNav/Users/UserSideNav";
-// import Modal from "react-modal";
-
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)"
-  }
-};
+import Modal from "react-responsive-modal";
 
 class DLScan extends Component {
   constructor() {
     super();
     this.state = {
-      modalIsOpen: false,
+      open: false,
       files: [],
       blinkResponse: []
     };
   }
-
-
-openModal() {
-  this.setState({ modalIsOpen: true })
-}
-
-  closeModal() {
-    this.setState({ modalIsOpen: false });
-  }
-
-  getFiles(files) {
+  getFiles = files => {
     this.setState({ files: files });
-  }
+  };
 
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
   submitHandler = () => {
-    // console.log(this.state.files[0].base64)
-
-    axios
-      .post("/api/dlscan", { base64: this.state.files[0].base64 })
-      .then(res => {
-        console.log(res.data);
-        this.setState({ blinkResponse: res.data });
-      });
-    this.openModal(this.state.blinkResponse)
+    console.log(this.state.files[0].base64);
+    // axios
+    //   .post("/api/dlscan", { base64: this.state.files[0].base64 })
+    //   .then(res => {
+    //     console.log(res.data);
+    //     this.setState({ blinkResponse: res.data });
+    //   });
+    // this.onOpenModal(this.state.blinkResponse)
   };
 
   render() {
-    console.log(this.state.blinkResponse)
-    const {customerFamilyName,
+    const { open } = this.state;
+    console.log(this.state.blinkResponse);
+    const {
+      customerFamilyName,
       customerFirstName,
       dateOfBirth,
       sex,
@@ -63,39 +50,24 @@ openModal() {
       height,
       fullAddress,
       jurisdictionVehicleClass,
-      documentExpirationDate} = this.state.blinkResponse
+      documentExpirationDate
+    } = this.state.blinkResponse;
     return (
       <div className="scan_main">
-      {/* <Modal
-        isOpen={this.state.modalIsOpen}
-        // onAfterOpen={this.afterOpenModal}
-        onRequestClose={this.closeModal}
-        style={customStyles}>
-
-          <div>{customerFamilyName}</div>
-          <div>{customerFirstName}</div>
-          <div>{dateOfBirth}</div>
-          <div>{sex}</div>
-          <div>{eyeColor}</div>
-          <div>{hairColor}</div>
-          <div>{weightPounds}</div>
-          <div>{height}</div>
-          <div>{fullAddress}</div>
-          <div>{jurisdictionVehicleClass}</div>
-          <div>{documentExpirationDate}</div>
-        </Modal> */}
-        <div className="left_container">
+      <div className="left_container">
           <UserSideNav />
         </div>
-
+        <button className="btn btn-action" onClick={this.onOpenModal}>
+          Open
+        </button>{" "}
+        
         <div className="scan_center">
-          <h1 className="text-center">Upload Drivers License</h1>
-          <br />
 
+        <h1 className="text-center">Upload Drivers License</h1>
+          <br />
           <div className="text-center mt-25">
             <FileBase64 multiple={true} onDone={this.getFiles.bind(this)} />
-          </div>
-          <br />
+          </div><br />
           <div className="text-center">
             {this.state.files.map((file, i) => {
               return (
@@ -112,18 +84,33 @@ openModal() {
           <button onClick={() => this.submitHandler()} className="scan_button">
             Submit
           </button>
-
-        
-
-          {/* {this.state.files.length !== 0 ? (
-            <div>
-              <h3 className="text-center mt-25">Callback Object</h3>
-              <div className="pre-container">
-                <pre>{JSON.stringify(this.state.files, null, 2)}</pre>
-              </div>
-            </div>
-          ) : null} */}
         </div>
+        <Modal open={open} onClose={this.onCloseModal} center>
+          <h1>Patric</h1>
+          <h1>Khiev</h1>
+          <h1>121688</h1>
+          <h1>sex 1</h1>
+          <h1>eye BRN</h1>
+          <h1>hair BRN</h1>
+          <h1>188</h1>
+          <h1>72 in</h1>
+          <h1>6 FALMOUTH COVE, SAN RAFAEL, CA 94901000</h1>
+          <h1>C</h1>
+          <h1>12162022</h1>
+          <button>Confirm</button>
+
+          {/* <div>{customerFamilyName}</div>
+          <div>{customerFirstName}</div>
+          <div>{dateOfBirth}</div>
+          <div>{sex}</div>
+          <div>{eyeColor}</div>
+          <div>{hairColor}</div>
+          <div>{weightPounds}</div>
+          <div>{height}</div>
+          <div>{fullAddress}</div>
+          <div>{jurisdictionVehicleClass}</div>
+          <div>{documentExpirationDate}</div> */}
+        </Modal>
       </div>
     );
   }
